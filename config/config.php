8d5c17e44bc62db7517ecd8ab9412d2ca8dcda95
Illia Volynets <volynets.i@hexide-digital.com>
2025-03-21 15:00:10 +0200

    fix: clean up unused code

--- config/config.php
+++ config/config.php
@@ -3,18 +3,7 @@
 return [
     'base_uri' => env('DOMENY_CORE_BASE_URI', ''),
 
-    'paths' => [
-        'domains' => [
-            'index' => 'api/v1/domains',
-        ],
-    ],
+    'lazy_loading' => true,
 
     'throw_exception_for_adapters' => false,
-    'adapters' => [
-        'user' => \Hexidedigital\DomenyCoreSdk\Classes\Adapters\Users\UserModelAdapter::class,
-        'profile' => \Hexidedigital\DomenyCoreSdk\Classes\Adapters\Users\ProfileModelAdapter::class,
-        'country' => \Hexidedigital\DomenyCoreSdk\Classes\Adapters\Country\CountryModelAdapter::class,
-        'city' => \Hexidedigital\DomenyCoreSdk\Classes\Adapters\City\CityModelAdapter::class,
-        'specialization' => \Hexidedigital\DomenyCoreSdk\Classes\Adapters\Specialization\SpecializationModelAdapter::class,
-    ],
 ];
